214510508934b84c6df0548836ea6eb8aa4bed5b
songah119 <skutcm119@gmail.com>
2022-07-15 08:38:14 +0900

    feat: custom hook 사용해서 모든 슬라이드에 observer 적용

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
 import styled from "styled-components";
 import SlideController from "./StController";
 import { useState, useEffect, useRef } from "react";
-// import { useThemeObserver } from "./useThemeObserver";
+import { useThemeObserver } from "./useThemeObserver";
 
 function App() {
   const [slideNumber, setSlideNumber] = useState(1);
-  const firstSlideRef = useRef(null);
-  const secondSlideRef = useRef();
-  const thirdSlideRef = useRef(null);
-  const fourthSlideRef = useRef(null);
 
-  // const [firstSlideRef] = useThemeObserver(setSlideNumber, 1);
-  // const [secondSlideRef] = useThemeObserver(setSlideNumber, 2);
-  // const [thirdSlideRef] = useThemeObserver(setSlideNumber, 3);
-  // const [fourthSlideRef] = useThemeObserver(setSlideNumber, 4);
-
-  const callBB = (entries, observer) => {
-    entries.forEach(async (entry) => {
-      if (entry.isIntersecting) {
-        //지정해준 entry=>2번째 슬라이드 가 intersecting될 때
-        setSlideNumber(2);
-        observer.observe(entry.target);
-      }
-    });
-  };
-
-  useEffect(() => {
-    let observer;
-    if (secondSlideRef.current) {
-      observer = new IntersectionObserver(callBB, {
-        threshold: 0.4,
-      });
-      observer.observe(secondSlideRef.current);
-    }
-    return () => {
-      observer && observer.disconnect();
-    };
-  }, []);
+  const firstSlideRef = useThemeObserver(setSlideNumber, 1);
+  const secondSlideRef = useThemeObserver(setSlideNumber, 2);
+  const thirdSlideRef = useThemeObserver(setSlideNumber, 3);
+  const fourthSlideRef = useThemeObserver(setSlideNumber, 4);
 
   return (
     <StContainer>
--- src/useThemeObserver.js
+++ src/useThemeObserver.js
@@ -3,21 +3,23 @@ import { useEffect, useRef } from "react";
 
 export const useThemeObserver = (setState, stateNumber) => {
   const isRef = useRef(null);
-  const option = {
-    rootMargin: "500px",
-    threshold: 1,
-  };
 
-  const callback = (entry) => {
-    if (entry[0].isIntersecting) {
-      setState(stateNumber);
-    }
+  const observePoint = (entries, observer) => {
+    entries.forEach(async (entry) => {
+      if (entry.isIntersecting) {
+        //props로 받은 슬라이드가 intersecting될 때
+        setState(stateNumber);
+        observer.observe(entry.target);
+      }
+    });
   };
 
   useEffect(() => {
     let observer;
     if (isRef.current) {
-      observer = new IntersectionObserver(callback, option);
+      observer = new IntersectionObserver(observePoint, {
+        threshold: 0.4,
+      });
       observer.observe(isRef.current);
     }
     return () => {
@@ -25,5 +27,5 @@ export const useThemeObserver = (setState, stateNumber) => {
     };
   }, []);
 
-  return [isRef];
+  return isRef;
 };
